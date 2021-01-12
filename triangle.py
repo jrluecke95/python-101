511266e887a814f52f09ba4cc4d844b679cc68f9
Jake Luecke <jrluecke95@gmail.com>
2021-01-12 16:56:38 -0500

    fixed bottom calc function

--- triangle.py
+++ triangle.py
@@ -1,10 +1,10 @@
 height = int(input("How tall do you want your triangle to be? "))
 def bottom_calc(height):
-    x = 1
-    for x in range(height):
-        x += 2
+    bot = 1
+    for x in range(height - 1):
+        bot += 2
         print(x)
-    return x
+    return bot
 
 bottom = bottom_calc(height)
 print(bottom)
