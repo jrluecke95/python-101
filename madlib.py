fae42fd3eaff784a3f799a3ecf23fac0cdd8ea1c
Jake Luecke <jrluecke95@gmail.com>
2021-01-12 15:26:12 -0500

    completed madlib

---
+++ madlib.py
@@ -0,0 +1,6 @@
+print("Please fill in the blanks below: ")
+print("__(name)__'s favorite subject is __(subject)__.")
+name = input("What is his name? ")
+subject = input("What is his favorite subject? ")
+
+print(name + "'s favorite subjet is " + subject + ".")
